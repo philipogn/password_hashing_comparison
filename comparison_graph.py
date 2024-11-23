0ec91bfba343fce69d26aeae7167144ceb170c58
Phillip <philip.ogn@gmail.com>
2024-11-23 22:34:06 +0000

    refactor: cleaned up code and adjusted parameters to match

--- comparison_graph.py
+++ comparison_graph.py
@@ -6,13 +6,10 @@ import time
 from argon2 import PasswordHasher
 import matplotlib.pyplot as plt
 
-
-# passwords = open('1000-most-common-passwords.txt').read().splitlines()
 passwords = open('password-list-top-100.txt').read().splitlines()
 
 def argon2_hash(passwords):
-    ph = PasswordHasher(time_cost=10, memory_cost=65536, parallelism=4)  # Higher time and memory cost, parallelism
-  # Adjusted parameters
+    ph = PasswordHasher(time_cost=1, memory_cost=47104, parallelism=1)
     for word in passwords:
         ph.hash(word)
 
@@ -20,13 +17,13 @@ def bcrypt_hash(passwords):
     for word in passwords:
         bcrypt.hashpw(word.encode(), bcrypt.gensalt(rounds=9))  # Adjusted rounds
 
+def scrypt_hash(passwords):
+    for word in passwords:
+        scrypt.hash(word, b'salt', N=2**17, r=8, p=1)
+
 def pbkdf2_hash(passwords):
     for word in passwords:
         hashlib.pbkdf2_hmac('sha256', word.encode(), b'salt', 600000)  # Adjusted iterations
-
-def scrypt_hash(passwords):
-    for word in passwords:
-        scrypt.hash(word, b'salt', N=2**14, r=8, p=5)
 
 # Measure execution time
 def measure_time(hash_func, words):
@@ -34,12 +31,6 @@ def measure_time(hash_func, words):
     hash_func(words)
     end_time = time.perf_counter()
     return end_time - start_time
-
-# def run_time():
-#     for func in [argon2_hash, bcrypt_hash, pbkdf2_hash, scrypt_hash]:
-#         exec_time = measure_time(func, passwords)
-#         memory_usage = psutil.Process().memory_info().rss / (1024 * 1024)  # convert to MB
-#         print(f'{func.__name__} - Time: {exec_time:.4f} seconds, Memory: {memory_usage:.2f} MB')
 
 # Run the test multiple times and average the results
 def measure_performance(hash_func, passwords, runs=5):
@@ -65,7 +56,7 @@ def measure_performance(hash_func, passwords, runs=5):
 # measure performance and plot
 def times_and_plot():
     algorithms = ['Argon2', 'Bcrypt', 'PBKDF2', 'Scrypt']
-    hash_funcs = [argon2_hash, bcrypt_hash, pbkdf2_hash, scrypt_hash]
+    hash_funcs = [argon2_hash, bcrypt_hash, scrypt_hash, pbkdf2_hash]
     avg_times = []
     avg_memories = []
 
@@ -90,8 +81,7 @@ def times_and_plot():
     ax2.set_ylabel('Memory Usage (MB)', color='red')
     ax2.tick_params(axis='y', labelcolor='red')
 
-    plt.title('Average Execution Time and Memory Usage of Password Hashing Algorithms')
+    plt.title('Execution Time and Memory Usage of Hashing a List of Passwords')
     plt.show()
 
-# run_time()
 times_and_plot()
